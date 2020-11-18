cdfaa1a6381a8fff728b13642e22ce7e293983ce
Dev-miniyoung <miniyoung37@gmail.com>
2020-11-18 15:43:59 +0900

    add assets images

--- creator_detail/src/assets/index.js
+++ creator_detail/src/assets/index.js
@@ -3,6 +3,7 @@ const images = {
   profileBackButton: require('./images/profileBackButton.png'),
   profileMoreButton: require('./images/profileMoreButton.png'),
   youtubeIcon: require('./images/youtubeIcon.png'),
+  triangleMoreButton: require('./images/triangleMoreButton.png')
 };
 
 export default images;
