6cf5190b0763368dfb56f83171b59dd6933d92c5
Dev-miniyoung <miniyoung37@gmail.com>
2020-11-18 15:43:50 +0900

    update api

---
+++ creator_detail/src/api.js
@@ -0,0 +1,15 @@
+import axios from 'axios';
+import apiKeys from './apiKeys';
+
+const api = axios.create({
+  baseURL: 'https://api.themoviedb.org/3/',
+  params: {
+    api_key: apiKeys,
+    language: 'en-US',
+  },
+});
+
+export const getDataApi = {
+  profile: () => api.get('person/287'),
+  dataList: () => api.get('movie/top_rated'),
+};
