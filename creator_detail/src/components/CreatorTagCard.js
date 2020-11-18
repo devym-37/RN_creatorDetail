76357957ccbfb336f5e4eb6c410c8042f4b73f09
Dev-miniyoung <miniyoung37@gmail.com>
2020-11-18 19:13:40 +0900

    add tagComponent

---
+++ creator_detail/src/components/CreatorTagCard.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  tagContainer: {
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  tagText: {
+    borderWidth: 1,
+    paddingRight: 10,
+    paddingLeft: 10,
+    paddingTop: 8,
+    paddingBottom: 8,
+    borderRadius: 5,
+    marginRight: 8,
+  },
+});
+
+const CreatorTagCard = () => {
+  return (
+    <View style={styles.tagContainer}>
+      <Text style={styles.tagText}>test</Text>
+    </View>
+  );
+};
+
+export default CreatorTagCard;
