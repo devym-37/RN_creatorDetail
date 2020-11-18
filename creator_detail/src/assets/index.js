537375f1e25ee19481b0cd3d30b95032d1d0702e
Dev-miniyoung <miniyoung37@gmail.com>
2020-11-18 13:58:22 +0900

    add assets images index file

---
+++ creator_detail/src/assets/index.js
@@ -0,0 +1,8 @@
+const images = {
+  profileBackGround: require('./images/profileBackGround.png'),
+  profileBackButton: require('./images/profileBackButton.png'),
+  profileMoreButton: require('./images/profileMoreButton.png'),
+  youtubeIcon: require('./images/youtubeIcon.png'),
+};
+
+export default images;
