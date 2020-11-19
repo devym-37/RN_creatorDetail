f192c9d0b2cccfc845b58ac627ad92df3b6527e1
Dev-miniyoung <miniyoung37@gmail.com>
2020-11-19 18:42:19 +0900

    add stack navigation

--- creator_detail/src/components/App.js
+++ creator_detail/src/components/App.js
@@ -3,13 +3,40 @@ import { SafeAreaView, StatusBar } from 'react-native';
 
 import CreatorMain from '../screens/CreatorMain';
 
+import { View, Text } from 'react-native';
+import 'react-native-gesture-handler';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+function HomeScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+    </View>
+  );
+}
+
+const Stack = createStackNavigator();
+
 const App = () => {
   return (
     <>
-      <StatusBar barStyle="dark-content" />
+      <SafeAreaView style={{ flex: 1 }}>
+        <NavigationContainer>
+          <Stack.Navigator>
+            <Stack.Screen
+              name="CreatorMain"
+              component={CreatorMain}
+              options={{ headerShown: false }}
+            />
+            <Stack.Screen name="Home" component={HomeScreen} />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </SafeAreaView>
+      {/* <StatusBar barStyle="dark-content" />
       <SafeAreaView style={{ flex: 1 }}>
         <CreatorMain />
-      </SafeAreaView>
+      </SafeAreaView> */}
     </>
   );
 };
